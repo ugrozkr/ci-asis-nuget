b498def4c66e2ec9100ecb0bec6efd824e819e6c
ugrozkr <ugrozkr@hotmail.com>
2017-07-25 16:29:47 +0300

    first commit test

---
+++ AsisDirectory/AsisDirectory/araclar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsisDirectory
+{
+    [Table("araclar")]
+    public class araclar
+    {
+        [Key]
+        public string plaka { get; set; }
+        public byte validatorVarMi { get; set; }
+        public Int32? validatorNo { get; set; }
+    }
+}
---
+++ AsisDirectory/AsisDirectory/binisAdetleri.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsisDirectory
+{
+    public class binisAdetleri
+    {
+        [Key]
+        public string BinisKarti { get; set; }
+        public int TamAdet { get; set; }
+        public decimal TamTutar { get; set; }
+        public int UltraLightAdet { get; set; }
+        public decimal UltraLightTutar { get; set; }
+        public int AbonmanAdet { get; set; }
+        public decimal AbonmanTutar { get; set; }
+        public int AktarmaAdet { get; set; }
+        public decimal AktarmaTutar { get; set; }
+        public int UcretsizAdet { get; set; }
+        public int ToplamAdet { get; set; }
+        public decimal ToplamTutar { get; set; }
+    }
+}
